bee61615f5f56670b852668542a86ae5b219d63a
Bastien KEREBEL <bastien.kerebel@gmail.com>
2021-03-14 21:31:42 +0100

    Ajoutez des fichiers projet.

---
+++ TP1Navire/Port/Port.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace TP1Navire
+{
+    class Port
+    {
+
+        private string nom;
+        private int nbNaviresMax = 5;
+        private List<Navire> navires = new List<Navire>();
+
+        public Port(string nom)
+        {
+            this.nom = nom;
+        }
+        public void EnregistrerArrivee(Navire navire)
+        {
+            if (nbNaviresMax > navires.Count)
+            {
+                navires.Add(navire);
+            }
+            else
+            {
+                throw new Exception("Ajout Impossible, le port est complet");
+            }
+        }
+        public void EnregistrerDepart(string imo)
+        {
+            if (EstPresent(imo))
+            {
+                navires.RemoveAt(RecupPosition(imo));
+            }
+            else
+            {
+                throw new Exception("Impossible d'enregistrer le navire" + imo + " , il n'est pas dans le port");
+            }
+        }
+        public void TesterRecupPosition()
+        {
+            this.EnregistrerArrivee(new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827));
+            this.EnregistrerArrivee(new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500));
+            this.EnregistrerArrivee(new Navire("IMO8715871", "MSC PILAR"));
+            String imo = "IMO9427639";
+            Console.WriteLine("Indice du navire" + imo + " dans la collection ; " + this.RecupPosition(imo));
+            imo = "IMO8715871";
+            Console.WriteLine("Indice du navire " + imo + " dans la collection : " + this.RecupPosition(imo));
+            imo = "IMO1111111";
+            Console.WriteLine("Indice du navire " + imo + " dans la collection : " + this.RecupPosition(imo)); ;
+        }
+        public void TesterRecupPositionV2()
+        {
+            Navire navire = new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827);
+            this.EnregistrerArrivee(navire);
+            this.EnregistrerArrivee(new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500));
+            this.EnregistrerArrivee(new Navire("IMO8715871", "MSC PILAR"));
+            Console.WriteLine("Indice du navire" + navire.Imo + " dans la collection : " + this.RecupPosition(navire));
+            Navire unAutreNavire = new Navire("IMQ8715871", "MSC PILAR");
+            Console.WriteLine("Indice du navire " + unAutreNavire.Imo + " dans la collection : " + this.RecupPosition(unAutreNavire));
+            unAutreNavire = new Navire("IMO8715871", "MSC PILAR", "Porte-conteneurs", 52181);
+            Console.WriteLine("Indice du navire " + unAutreNavire.Imo + " dans la collection : " + this.RecupPosition(unAutreNavire)); ;
+        }
+        public bool EstPresent(string imo)
+        {
+            foreach (Navire navire in navires)
+            { 
+                if ( imo == navire.Imo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private int RecupPosition(string imo)
+        {
+            
+           if ( EstPresent(imo))
+           {
+                foreach (Navire navire in navires)
+                {
+                    if (imo == navire.Imo)
+                    {
+                        return RecupPosition(navire);
+                    }
+                   
+                }
+            }
+            return -1;
+        }
+        private int RecupPosition(Navire navire)
+        {
+                if (navires.Contains(navire))
+                {
+                    return navires.IndexOf(navire);
+                }
+            return -1;
+        }
+        public string Nom { get => nom; }
+        public int NbNaviresMax { get => nbNaviresMax; set => nbNaviresMax = value; }
+    }
+}
---
+++ TP1Navire/Programme/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace TP1Navire
+{
+    class Program
+    {
+        static void Main()
+        {
+            ////TesterInstanciations();
+            ////TesterEnregistrerArrivee();
+            ////TesterRecupPosition();
+            //TesterRecupPositionV2();
+            //TesterEnregistrerDepart();
+            testerEstPresent();
+        }
+        public static void TesterInstanciations()
+        {
+            //déclaration de l'objet unNavire de la classe Navire
+            Navire unNavire;
+            // Instanciation de l'objet
+            unNavire = new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827);
+            Affiche(unNavire);
+            //Declaration ET instanciation d'un autre objet de la classe Navire
+            Navire unAutreNavire = new Navire("IMO9427639", "MSC Isabella", "Porte-conteneurs", 197500);
+            Affiche(unAutreNavire);
+            // Instanciation de l'objet
+            unAutreNavire = new Navire("IMO8715871", "MSC PILAR");
+            Affiche(unAutreNavire);
+        }
+        public static void Affiche(Navire navire)
+        {
+            //Console.WriteLine("Identification : " + navire.Imo);
+            //Console.WriteLine("Nom : " + navire.Nom);
+            //Console.WriteLine("Type de frêt : " + navire.LibelleFret);
+            //Console.WriteLine("Quantité de frêt :        " + navire.QteFretMaxi);
+            Console.WriteLine(navire);
+            Console.WriteLine("--------------------------");
+        }
+        public static void TesterEnregistrerArrivee()
+        {
+            try
+            {
+                Port port = new Port("Brest");
+                port.EnregistrerArrivee(new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827));
+                port.EnregistrerArrivee(new Navire("IMO8715871", "MSC PILAR"));
+                port.EnregistrerArrivee(new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500));
+                port.EnregistrerArrivee(new Navire("IMO5548482", "MSC FRAER", "Porte-conteneurs", 198200));
+                port.EnregistrerArrivee(new Navire("IMO3547957", "Silver Spirit", "Hydrocarbures", 176350));
+                port.EnregistrerArrivee(new Navire("IMO1486784", "MSC TOWER"));
+                Console.WriteLine("Navires bien enregistrés dans le port"); ;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void TesterRecupPosition()
+        {
+            (new Port("Toulon")).TesterRecupPosition();
+        }
+        public static void TesterRecupPositionV2()
+        {
+            (new Port("Toulon")).TesterRecupPositionV2();
+        }
+        public static void TesterEnregistrerDepart()
+        {
+            try
+            {
+                Port port = new Port("Toulon");
+                port.EnregistrerArrivee(new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827));
+                port.EnregistrerArrivee(new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500));
+                port.EnregistrerArrivee(new Navire("IMO8715871", "MSC PILAR"));
+                port.EnregistrerDepart("IMO8715871");
+                Console.WriteLine("Départ du navire IMO871871 enregistré");
+                port.EnregistrerDepart("IMO1111111");
+                Console.WriteLine("Départ du navire IMO1111111 enregistré");
+                Console.WriteLine("fin des enregistrements des départs");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void testerEstPresent()
+        {
+            Port port = new Port("Toulon");
+            port.EnregistrerArrivee(new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827));
+            port.EnregistrerArrivee(new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500));
+            port.EnregistrerArrivee(new Navire("IMO8715871", "MSC PILAR"));
+            String imo = "IMO9427639";
+            Console.WriteLine("Le navire " + imo + "est présent dans le port : " + port.EstPresent(imo));
+            imo = "IMO11111111";
+            Console.WriteLine("Le navire " + imo + "est présent dans le port : " + port.EstPresent(imo));
+        }
+    }
+}
